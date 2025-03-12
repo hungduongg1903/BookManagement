0c2d6caba12bb15bad1271eb29e7a7632b08453f
ManhNTHE170803 <manhnthe170803@fpt.edu.vn>
2025-03-12 19:16:44 +0700

    API category
    6 APi category

--- BE/routers/index.js
+++ BE/routers/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const bookRouter = require("./book.js");
 // const authorRouter = require("./author.js");
-// const categoryRouter = require("./category.js");
+const categoryRouter = require("./category.js");
 // const userRouter = require("./user.js");
 // const loanRouter = require("./loan.js");
 
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.use("/books", bookRouter);
 // router.use("/authors", authorRouter);
-// router.use("/categories", categoryRouter);
+router.use("/categories", categoryRouter);
 // router.use("/users", userRouter);
 // router.use("/loans", loanRouter);
 
