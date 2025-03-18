7f9b2e07f1fefe18deb0efd370df13b559e00570
thih-n <dongnguyen1968hldh@gmail.com>
2025-03-18 15:47:55 +0700

    uu

--- BE/routers/index.js
+++ BE/routers/index.js
@@ -3,22 +3,17 @@ const express = require("express");
 const bookRouter = require("./book.js");
 const authorRouter = require("./author.js");
 const categoryRouter = require("./category.js");
-// const userRouter = require("./user.js");
-// const loanRouter = require("./loan.js");
+const userRouter = require("./user.js");
+const loanRouter = require("./loan.js");
 
 const router = express.Router();
 
 router.use("/books", bookRouter);
 router.use("/authors", authorRouter);
 router.use("/categories", categoryRouter);
-// router.use("/users", userRouter);
-// router.use("/loans", loanRouter);
-const loanRoutes = require("./loan"); // Add this line
-router.use("/loans", loanRoutes); // Add this line
-// Mount other routers if they exist
-// router.use("/authors", authorRouter);
-// router.use("/categories", categoryRouter);
-// router.use("/users", userRouter);
+
+router.use("/loans", loanRoutes); 
+router.use("/users", userRouter);
 
 // User loans route can be added to userRouter or handled here
 // router.get("/users/:id/loans", authenticate, loanController.getUserLoans);
